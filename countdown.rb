fffa86ac5c2aaeea67a7a8348b5f8b99366a826b
Nabil Hayet <nabil.bracu@gmail.com>
2020-03-28 01:57:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number=number-1
 end
+def countdown_with_sleep
+  1.times do
+   sleep(5)
+  end 
+  end
+return "HAPPY NEW YEAR!"
+end
+
+countdown(10)
