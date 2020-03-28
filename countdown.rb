596e0d11b66beaf796e6d4b56dfc7f816bc93479
Nabil Hayet <nabil.bracu@gmail.com>
2020-03-28 02:00:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number=number-1
 end
+def countdown_with_sleep(num)
+  while(num > 0)
+   sleep(1)
+   num=num-1
+  end 
+  end
+return "HAPPY NEW YEAR!"
+end
+
+countdown(10)
+countdown_with_sleep(5)
